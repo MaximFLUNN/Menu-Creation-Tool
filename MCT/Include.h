186543f2088f722a5dc126a65ebac60fdfd32165
Филатов Максим <999099917@mail.ru>
2023-08-17 23:48:04 +0300

    Added inludes

--- MCT/Include.h
+++ MCT/Include.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
+#include <vector>
+#include <random>
+#include <thread>
+
+#include "Helpers/Defines.h"
+#include "Helpers/Colors.h"
+#include "Helpers/Converter.h"
+#include "Helpers/Enums.h"
+
+#include "KeyboardListener/KeyboardFlags.h"
+#include "KeyboardListener/KeyboardSpecial.h"
+#include "KeyboardListener/KeyboardListener.h"
+
+#include "Units/Secondary/CurrentSelection.h"
+#include "Units/Secondary/PaintKit.h"
+#include "Units/Secondary/Terminal.h"
+
+#include "Units/Major/MenuUnitArg.h"
+#include "Units/Major/PageUnit.h"
+#include "Units/Major/MenuPage.h"
+#include "Units/Major/MCT.h"
